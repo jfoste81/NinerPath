564f2610b8df0a52b45bcc25afe0c68e917185a9
jfoste81 <jfoste81@uncc.edu>
2026-04-13 00:17:28 -0400

    Fixing URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_BASE ?? 'https://ninerpath.onrender.com/';
+export const API_BASE = import.meta.env.VITE_API_BASE ?? 'https://ninerpath.onrender.com';
 
 export const CONCENTRATIONS_BY_DEGREE = {
   bs_computer_science: [
