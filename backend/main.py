4210fd19ffa86e1af019d0fd27c589809811a138
jfoste81 <jfoste81@uncc.edu>
2026-04-13 00:13:28 -0400

    Updating CORS middleware URL

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ app = FastAPI(lifespan=_lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://ninerpath.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
